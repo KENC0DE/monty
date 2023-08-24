05f2d600995793d1cc1313e1ee57716c23287823
KENC0DE <kenansameseret@gamil.com>
2023-08-25 01:30:14 +0530

    rotl

---
+++ opration_function_3.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * rotl_f - rotate the stack down.
+ * @stack: the stack.
+ * @line_number: the line number.
+*/
+void rotl_f(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *tp = *stack;
+	stack_t *tmp = *stack;
+
+	if (!tp)
+		return;
+
+	*stack = (*stack)->next;
+	while (tp->next)
+		tp = tp->next;
+
+	tp->next = tmp;
+	tmp->next = NULL;
+}
